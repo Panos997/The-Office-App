922858d391e5f1dbb26084bb268e0b85aacc02d0
Panos Antoniozas <145284714+Panos997@users.noreply.github.com>
2026-02-16 15:18:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,5 +8,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Τρέχει στην πόρτα 5000
-    app.run(debug=True, port=5000)
+    # Το Render χρησιμοποιεί την περιβαλλοντική μεταβλητή PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
